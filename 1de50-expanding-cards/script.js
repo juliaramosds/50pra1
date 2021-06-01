fa38aa6079d9b7f2adc98f661e20dd369cc2244f
Julia Ramos <julia.souza@ahgora.com.br>
2021-05-31 23:06:14 -0300

    1de50 expanding cards

--- 1de50-expanding-cards/script.js
+++ 1de50-expanding-cards/script.js
@@ -1,3 +1,14 @@
 const panels = document.querySelectorAll('.panel')
 
-//for pra cada div clicada tirar o active e adicionar se nao tiver
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
